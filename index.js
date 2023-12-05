629b426207a97ef63f2c592295edf0eb3fefe523
rishirathoreee <rishi.codemeg@gmail.com>
2023-12-05 19:15:10 +0530

    added changes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 const app = express()
 
+
+app.use('/',(req,res)=>{
+    console.log('running')
+    return res.send('sending ressssponddddd')
+})
+
 app.listen(8000,()=>{
     console.log('server running')
 })
